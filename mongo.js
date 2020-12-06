3d007d3898b98b9c2f8d81483a279c915b4b821b
Anoop S Mohan <anoop4439@gmail.com>
2020-12-06 11:54:42 +0530

    Sample mongo DB project initial commit

---
+++ mongo.js
@@ -0,0 +1,14 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb+srv://anoop:<password>@cluster0.62frb.mongodb.net/products_test?retryWrites=true&w=majority'
+
+const createProducts = async (req, res, next) => {
+
+};
+
+const getProducts = async (req, res, next) => {
+
+};
+
+exports.createProducts = createProducts;
+exports.getProducts = getProducts;
