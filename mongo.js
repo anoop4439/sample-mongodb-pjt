46f9e6712fc5f9deaae95d4213d38886d9adf0c7
Anoop S Mohan <anoop4439@gmail.com>
2020-12-09 20:42:00 +0530

    DB connection and data entry added

--- mongo.js
+++ mongo.js
@@ -1,9 +1,25 @@
 const MongoClient = require('mongodb').MongoClient;
 
-const url = 'mongodb+srv://anoop:<password>@cluster0.62frb.mongodb.net/products_test?retryWrites=true&w=majority'
+const url = 'mongodb+srv://<username>:<password>@cluster0.62frb.mongodb.net/<database>?retryWrites=true&w=majority'
 
 const createProducts = async (req, res, next) => {
+    const newProduct = {
+        name: req.body.name,
+        price: req.body.price
+    };
 
+    const client = new MongoClient(url);
+
+    try {
+        await client.connect();
+        const db = client.db();
+        const result = db.collection('products').insertOne(newProduct);
+    } catch (error) {
+        return res.json({message: 'Could not save data'});
+    }
+    client.close();
+
+    res.json(newProduct);
 };
 
 const getProducts = async (req, res, next) => {
